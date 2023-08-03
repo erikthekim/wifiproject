5389d780b43b188685f77270ab04cffdfff382e1
James Skon <skonjp@kenyon.edu>
2023-08-03 20:55:12 +0100

    Add directory for install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #  Install start code for wifi
 # James Skon
 # Copyright 2023
+sudo mkdir -p startcloudwifi.sh /root/.scripts
 sudo cp startcloudwifi.sh /root/.scripts
 sudo cp cloudwifi.service /lib/systemd/system -v
 sudo systemctl daemon-reload
