3c90b544dd6b9e592a2fc621fd40bf2feaec1ec7
James Skön <skonjp@kenyon.edu>
2024-03-21 11:20:00 -0400

    Update wifi_state_machine.rb
    Add Default time

--- wifi_state_machine.rb
+++ wifi_state_machine.rb
@@ -23,6 +23,9 @@ PORT=3000   #default
 #PORT=3003
 #PORT=3004
 
+# Default Sleep Time in seconds
+DEFAULT_SLEEP = 30
+
 #require 'active_support/core_ext/object/blank'
 
 # Wifi State Machine Code
@@ -424,7 +427,7 @@ end
 #
 #################################################################
 class State
-  def initialize(state, sleep_time = 5)
+  def initialize(state, sleep_time = DEFAULT_SLEEP)
     @my_state=state
     @changed=true
     @last_run_time_ms = 0
